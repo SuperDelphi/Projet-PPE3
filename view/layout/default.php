87cb41d9f602b391c0c6a1b45efeddcb87c8ce0c
unknown <theo.forgit17@gmail.com>
2019-10-16 15:54:59 +0200

    Création du footer dans le layout default.php
    Création du footer en HTML

--- view/layout/default.php
+++ view/layout/default.php
@@ -1,16 +1,40 @@
 <!DOCTYPE html>
 
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-        <title><?php echo isset($title_for_layout) ? $title_for_layout : ""; ?></title>
-    </head>
-    <body>
-        <header>
-            <h1>T2T</h1>
-        </header>
-        <section>
-            <?= $content_for_layout ?>
-        </section>
-    </body>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title><?php echo isset($title_for_layout) ? $title_for_layout : ""; ?></title>
+</head>
+
+<body>
+    <header>
+        <h1>T2T</h1>
+    </header>
+    <section>
+        <?= $content_for_layout ?>
+    </section>
+</body>
+<footer>
+    <h4>Liens utiles</h4>
+    <ul>
+        <li>Plan du site</li>
+        <li>Contact</li>
+        <li>F.A.Q</li>
+    </ul>
+
+    <h4>Mentions légales</h4>
+    <ul>
+        <li>C.G.U</li>
+        <li>Politique de confidentialité</li>
+    </ul>
+
+    <h4>Mentions légales</h4>
+    <p>Résidence Club La Fayette, Avenue de Bourgogne</p>
+    <p>17401 La Rochelle (CEDEX 01)</p>
+    <p>05 46 41 73 13</p>
+    <p>ufolep-usep@laligue17.org</p>
+
+</footer>
+
 </html>
