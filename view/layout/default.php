b8d8227e0cd2b1aa9e1453a7bf5362f3bfe7e501
unknown <theo.forgit17@gmail.com>
2019-10-16 16:53:17 +0200

    élaboration du footer
    début de la création du footer avec bootstrap

--- view/layout/default.php
+++ view/layout/default.php
@@ -5,7 +5,8 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <title><?php echo isset($title_for_layout) ? $title_for_layout : ""; ?></title>
-    <link rel="stylesheet" href="../../webroot/css/footer.css">
+    <link rel="stylesheet" href="<?php echo BASE_SITE . DS . 'css/footer.css' ?>">
+    <link rel="stylesheet" href='<?php echo BASE_SITE . DS . '/bootstrap/css/bootstrap.css' ?>' rel="stylesheet">
 </head>
 
 <body>
@@ -16,6 +17,7 @@
         <?= $content_for_layout ?>
     </section>
 </body>
+<!--
 <footer>
     <h4>Liens utiles</h4>
     <ul>
@@ -37,5 +39,75 @@
     <p>ufolep-usep@laligue17.org</p>
 
 </footer>
+-->
+<!-- Footer -->
+<footer class="page-footer font-small blue pt-4">
+
+    <!-- Footer Links -->
+    <div class="container-fluid text-center text-md-left">
+
+        <!-- Grid row -->
+        <div class="row">
+
+
+            <!-- Grid column -->
+
+            <hr class="clearfix w-100 d-md-none pb-3">
+
+            <!-- Grid column -->
+            <div class="col-md-3 mb-md-0 mb-3">
+
+                <!-- Links -->
+                <h5 class="text-uppercase">Liens utiles</h5>
+
+                <ul class="list-unstyled">
+                    <li>
+                        <a href="#!">Plan du site</a>
+                    </li>
+                    <li>
+                        <a href="#!">Contact</a>
+                    </li>
+                    <li>
+                        <a href="#!">F.A.Q</a>
+                    </li>
+                </ul>
+
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-3 mb-md-0 mb-3">
+
+                <!-- Links -->
+                <h5 class="text-uppercase">Mentions légales</h5>
+
+                <ul class="list-unstyled">
+                    <li>
+                        <a href="#!">C.G.U</a>
+                    </li>
+                    <li>
+                        <a href="#!">Politique de confidentialité</a>
+                    </li>
+                </ul>
+
+            </div>
+            <!-- Grid column -->
+            <!-- Grid column -->
+            <div class="col-md-6 mt-md-0 mt-3">
+
+                <!-- Content -->
+                <h5 class="text-uppercase">L'UFOLEP 17 </h5>
+                <p>Résidence Club La Fayette, Avenue de Bourgogne 17401 La Rochelle (CEDEX 01)
+                    <br>05 46 41 73 13
+                    <br>ufolep-usep@laligue17.org</p>
+                <br><br>
+            </div>
+        </div>
+        <!-- Grid row -->
+
+    </div>
+    <!-- Footer Links -->
+</footer>
+<!-- Footer -->
 
 </html>
