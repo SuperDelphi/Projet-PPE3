39c81a4fdf46eee1a0b1de18dc815505607e6fa5
kemkemm <kaezer17390@gmail.com>
2019-10-16 16:53:08 +0200

    Commencement de la navbar
    Début de création de la navbar dans le default.php + création de header.css pour faire le style de la navbar

--- view/layout/default.php
+++ view/layout/default.php
@@ -4,10 +4,21 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
         <title><?php echo isset($title_for_layout) ? $title_for_layout : ""; ?></title>
+
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href='<?php echo BASE_SITE . DS . '/bootstrap/css/bootstrap.css' ?>' rel="stylesheet">
+
     </head>
     <body>
         <header>
-            <h1>T2T</h1>
+            <!-- <h1>T2T</h1> -->
+            <!--Navbar
+            <nav class="navbar navbar-expand-sm navbar-dark" style='background-color: #002752'>
+                
+                <form class="form-inline" action="#">
+                    <input class="form-control" type="text" size="25" placeholder="Rechercher un championnat, un joueur etc">
+                </form>
+            </nav>-->
         </header>
         <section>
             <?= $content_for_layout ?>
