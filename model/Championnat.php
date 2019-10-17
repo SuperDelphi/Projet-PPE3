0a8c58227efd55365efef5b61b52f01801b830b1
Raphabuleux <castillophotos@gmail.com>
2019-10-17 16:50:43 +0200

    Mis à jour Championnat.php
    Il n'y a plus la sale erreur php lorsqu'on arrive sur la page \o/
    
    Co-Authored-By: starmaster007 <starmaster007@users.noreply.github.com>

--- model/Championnat.php
+++ model/Championnat.php
@@ -1,5 +1,5 @@
 <?php
 class Championnat extends Model {
-    var $table = " inner join championnat.idChampionnat on equipe = equipe.idEquipe  " ;
+    var $table = "championnat inner join engagement on championnat.idChampionnat = engagement.idChampionnat inner join equipe on equipe.idEquipe = engagement.idEquipe " ;
 }
 ?>
