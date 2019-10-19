eff3c2bd8b63631fb28610e443718ae61b08f460
Simon <simon.breil42@gmail.com>
2019-10-19 15:13:42 +0200

    Update includes.php
    Inclusion de la classe Security.

--- core/includes.php
+++ core/includes.php
@@ -1,9 +1,15 @@
 <?php
 
+// Fichier de configuration
 require ROOT . DS . 'config' . DS . 'conf.php';
+
+// Cœurs
 require 'Router.php';
 require 'Request.php';
 require 'Dispatcher.php';
 require 'Controller.php';
 require 'Model.php';
 require 'Session.php';
+
+// Scripts
+require "../scripts/Security.php";
