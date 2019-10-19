46c8d156b0faaae797b01338ecbfb04709d2543b
Simon <simon.breil42@gmail.com>
2019-10-19 15:17:59 +0200

    Ignorance de tous les fichiers XML

--- model/Arbitre.php
+++ model/Arbitre.php
@@ -1,15 +1,16 @@
 <?php
 
-class Arbitre extends Personne
+class Arbitre extends Compte
 {
     
     var $table = "arbitre inner join personne on arbitre.idArbitre = personne.idPersonne";
     
     private $mdp;
 
-    public function __construct($nom, $prenom, $age, $mail, $adresse, $mdp)
+    public function __construct($nom, $prenom, $age, $mail, $adresse, $mdp, $identifiant)
     {
         parent::__construct($nom, $prenom, $age, $mail, $adresse);
         $this->mdp = $mdp;
+        $this->identifiant = $identifiant;
     }
 }
--- model/Compte.php
+++ model/Compte.php
@@ -3,5 +3,14 @@
 
 class Compte extends Personne
 {
+    // Les gérants sont représentés par la classe Compte, dont hérite la classe Arbitre.
+    var $table = "compte INNER JOIN personne ON compte.idCompte = personne.idPersonne";
 
+    private $mdp;
+
+    public function __construct($nom, $prenom, $age, $mail, $adresse, $mdp)
+    {
+        parent::__construct($nom, $prenom, $age, $mail, $adresse);
+        $this->mdp = $mdp;
+    }
 }
