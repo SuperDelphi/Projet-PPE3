cf62e6cd301485d04507c3a4e7dcb3add3d148da
kemkemm <kaezer17390@gmail.com>
2019-11-06 11:26:01 +0100

    Ajout redirection
    Ajout de la redirection vers la base du site lorsqu'un utilisateur clique sur le logo dans le header

--- view/layout/default.php
+++ view/layout/default.php
@@ -15,7 +15,7 @@
 <body>
 <header>
     <nav class="navbar navbar-expand-lg">
-        <a class="navbar-brand link" href="#">UFOLEP 17</a>
+        <a class="navbar-brand link" href="<?php echo BASE_URL; ?>">UFOLEP 17</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
