bfc4041cb696fdba988b7fbdeebbdcc0c2b1bf2a
StarMaster007 <55792130+StarMaster007@users.noreply.github.com>
2019-11-06 15:52:08 +0100

    Ajout méthode liste championnat

--- controller/ChampionnatController.php
+++ controller/ChampionnatController.php
@@ -4,8 +4,8 @@ class ChampionnatController extends Controller {
 
     function liste() {
         $this->modChamp = $this->loadModel('Championnat');
-        $d['championnat'] = $this->modChamp->find(array('conditions' => 1));
-        if (empty($d['championnat'])) {
+        $d['championnats'] = $this->modChamp->find(array('conditions' => 1));
+        if (empty($d['championnats'])) {
             $this->e404('Page introuvable');
         }
         $this->set($d);
---
+++ view/championnat/liste.php
@@ -0,0 +1,21 @@
+<section >
+    <form method="post" action="<?= BASE_URL ?>">
+        <table border=1 style="text-align : center">
+            <thead>
+                <tr>
+                    <th>Nom</th>
+                    <th>Type</th>
+                    <th>Division</th>
+                </tr>
+            </thead>
+            <?php foreach ($championnats as $c) : ?>
+                <tr>
+                    <td><a href="<?php echo BASE_URL . '/joueur/detail/' . $c->nomChampionnat; ?>" 
+                           title="Cliquez pour modifier"><?= $c->nomChampionnat ?></a></td>
+                    <td> <?= $c->typeChampionnat ?></td>
+                    <td> <?= $c->nomDivision ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
+    </form>
+</section>
