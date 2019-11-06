c92947e2d2dc77b04b9cea66a427c2d3cf328134
Simon <simon.breil42@gmail.com>
2019-11-06 15:16:03 +0100

    Création de la méthode "formChampionnat"

--- controller/AdminController.php
+++ controller/AdminController.php
@@ -10,9 +10,25 @@ class AdminController extends Controller
 
     function formChampionnat()
     {
-        $divisionModele = $this->loadModel("division");
-        $d["divisions"] = $divisionModele->find();
-        $this->set($d);
-        $this->render("formChampionnat");
+        if (isset($_POST["creerChampionnat"])) {
+            $championnatModele = $this->loadModel("Championnat");
+
+            $nomChampionnat = $_POST["nomChampionnat"];
+            $typeChampionnat = $_POST["typeChampionnat"];
+            $nombreJournees = $_POST["nombreJournee"]; // TODO S'occuper de ça
+            $idDivision = $_POST["idDivision"];
+
+            $championnatModele->insertAI(
+                ["nomChampionnat", "typeChampionnat", "idDivision"],
+                [$nomChampionnat, $typeChampionnat, $idDivision]
+            );
+
+            $this->render("listeChampionnat");
+        } else {
+            $divisionModele = $this->loadModel("division");
+            $d["divisions"] = $divisionModele->find();
+            $this->set($d);
+            $this->render("formChampionnat");
+        }
     }
 }
