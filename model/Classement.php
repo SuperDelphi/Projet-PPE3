3d5e1c156bd67e43cd3e4f5563cbc60cacf07128
kemkemm <kaezer17390@gmail.com>
2019-11-06 14:15:29 +0100

    Update Classement.php
    Modification bug

--- model/Classement.php
+++ model/Classement.php
@@ -2,7 +2,6 @@
 
 class Classement extends Model {
     var $table = "equipe inner join division on equipe.idDivision = division.idDivision inner join championnat on division.idDivision = championnat.idDivision" ;
-    // championnat inner join engagement on championnat.idChampionnat = engagement.idChampionnat inner join equipe on equipe.idEquipe = engagement.idEquipe 
 }
 ?>
 
