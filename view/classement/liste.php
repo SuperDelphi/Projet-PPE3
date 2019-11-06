944e828b409e73404af23f2dae29cbfe229f90a8
Risuro <risemaj@gmail.com>
2019-11-06 16:05:24 +0100

    Controller TODO

--- view/classement/liste.php
+++ view/classement/liste.php
@@ -5,21 +5,12 @@
                 <tr>
                     <th>1</th>
                     <th>2</th>
-                    <th>3</th>
-                    <th>4</th>
-                    <th>5</th>
-                    <th>6</th>
-
                 </tr>
             </thead>
             <?php foreach ($classement as $c) : ?>
                 <tr>
-                    <td><?= $c->idEquipe ?></td>
                     <td><?= $c->nomEquipe ?></td>
-                    <td><?= $c->idDivision ?></td>
-                    <td><?= $c->nomDivision ?></td>
-                    <td><?= $c->idChampionnat ?></td>
-                    <td><?= $c->nomChampionnat ?></td>
+                    <td><?= $c->scoreGlobal ?></td>
                 </tr>
 
             <?php endforeach; ?>
