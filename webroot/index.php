5f0383efb5660fc56a0f31a3bfe8b2b8c71a11c3
StarMaster007 <55792130+StarMaster007@users.noreply.github.com>
2019-11-06 15:40:29 +0100

    FIX INDEX.PHP pour serveur virtuel

--- webroot/index.php
+++ webroot/index.php
@@ -11,7 +11,11 @@ define('SERVER', $_SERVER['HTTP_HOST']);
 
 define('DS', DIRECTORY_SEPARATOR);
 define('CORE', ROOT . DS . 'core');
-define('BASE_URL', dirname(dirname($_SERVER['SCRIPT_NAME'])));
+
+$tmp = (dirname(dirname($_SERVER['SCRIPT_NAME'])));
+
+//Si la base est / les formes ont une mauvaise redirection
+define('BASE_URL', $tmp === '/' ? '' : $tmp);
 define('BASE_SITE', dirname($_SERVER['SCRIPT_NAME']));
 
 include CORE . DS . 'includes.php';
