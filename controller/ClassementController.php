1c66e0b463cec27a2414fa7d01b393b38044f9fb
kemkemm <kaezer17390@gmail.com>
2019-11-06 14:11:47 +0100

    Commencement du classement
    Création du model, controller et vue de classement

---
+++ controller/ClassementController.php
@@ -0,0 +1,23 @@
+<?php
+
+class ClassementController extends Controller
+{
+
+    private $modClassement = null;
+
+    function liste()
+    {
+        /*$this->modArbitre = $this->loadModel('Arbitre');
+        $d['arbitres'] = $this->modArbitre->find(array('conditions' => 1));
+        if (empty($d['arbitres'])) {
+            $this->e404('Page introuvable');
+        }
+        $this->set($d);*/
+    }
+    
+    function tri() {
+        
+    }
+    
+}
+
