a8a17eafac2f9da9b16b266e38fb846ea60a4cf2
kemkemm <kaezer17390@gmail.com>
2019-11-13 16:53:29 +0100

    Update listeChampionnat.php

--- view/admin/listeChampionnat.php
+++ view/admin/listeChampionnat.php
@@ -1,5 +1,6 @@
 <!-- Lien provisoire -->
 <br>
+<<<<<<< HEAD
 <div class="container-fluid" style="height:150px">
     <div class="row w-25 h-25 border border-dark text-center">
         <div class="col">
@@ -19,3 +20,22 @@
         </div>
     </div>
 </div>
+=======
+
+<div class="row">
+    <div class="col-sm-2">
+        <a href="<?= BASE_URL . DS ?>admin/formChampionnat">Nouveau championnat</a>
+    </div>
+    <div class="col-sm-2">
+        en cours
+    </div>
+</div>
+<div>
+    <a href="<?= BASE_URL . DS ?>admin/modifChampionnat">Modifier un championnat existant</a>
+</div>
+<div class="row">
+    <div class="col-sm">
+        <a href="<?= BASE_URL . DS ?>auth/logout">Déconnexion</a>
+    </div>
+</div>
+>>>>>>> 9f1d6d7c2c1e6d0d7e706ace2b637a7d4b359cda
