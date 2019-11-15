68900e6fb846231cd7d6c6ec418573fd3a0c5520
StarMaster007 <55792130+StarMaster007@users.noreply.github.com>
2019-11-15 13:50:23 +0100

    FIX Affichage liste des rencontres
    Ajout d'un lien vers les détails

--- view/rencontre/liste.php
+++ view/rencontre/liste.php
@@ -1,12 +1,14 @@
 <h2><?= $championnat->nomChampionnat . '<br>' . $championnat->typeChampionnat . '<br>' . $championnat->nomDivision; ?></h2>
 <?php $cpt = 0;
 foreach ($journee as $j) :
-    $cpt++;
+    if ($cpt > 10) {
+    $cpt = 0;
+}
+$cpt++;
 foreach ($j as $rencontre) :
-    if ($rencontre > 0 && $rencontre < 10) { ?>
+    if ($rencontre > 0 && $rencontre < 100) { ?>
         <div style=''>
-            <table border='1' style='text-align:center;width: 30%'>
-                
+            <table border='1' style='text-align:center;width: 45%'>
 <?php 
 } else {
     if (gettype($rencontre) == "string") { ?>
@@ -15,21 +17,19 @@ foreach ($j as $rencontre) :
         </thead>
 <?php 
 } else {
-    if ($rencontre < 1 && $rencontre > 10) {
+    if ($rencontre < 1 && $rencontre > 100) {
     } else {
-                    //echo $rencontre;
         foreach ($rencontre as $r) : ?>
-
                 <tr>
-                    <td style='width:40%'>
+                    <td style='width:40%'><a href="<?php echo BASE_URL . '/rencontre/detail/' . $r->idRencontre; ?>">
                     <?php foreach ($equipes as $e) {
                         if ($e->idEquipe == $r->idEquipeA) {
                             echo $e->nomEquipe;
                         }
-                    } ?></td>
-                    <td style='width:5%'> <?= isset($r->scoreFinalA) ? $j->scoreFinalA : '?' ?> </td>
+                    } ?></a></td>
+                    <td style='width:5%'> <?= isset($r->scoreFinalA) ? $r->scoreFinalA : '?' ?> </td>
                     <td style='width:5%'> - </td>
-                    <td style='width:5%'> <?= isset($r->scoreFinalB) ? $j->scoreFinalB : '?' ?> </td>
+                    <td style='width:5%'> <?= isset($r->scoreFinalB) ? $r->scoreFinalB : '?' ?> </td>
                     <td style='width:40%'>
                     <?php foreach ($equipes as $e) {
                         if ($e->idEquipe == $r->idEquipeB) {
@@ -40,7 +40,8 @@ foreach ($j as $rencontre) :
                 </tr> <?php endforeach; ?>
             </table>
         </div>
-
+        <br>
+        <br>
                 <?php 
             }
         }
