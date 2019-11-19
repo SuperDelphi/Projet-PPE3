73f9ec17ddfd8d82927738811bc2127afec334f7
kemkemm <kaezer17390@gmail.com>
2019-11-19 11:55:13 +0100

    Update formRencontre.php

--- view/admin/formRencontre.php
+++ view/admin/formRencontre.php
@@ -20,25 +20,6 @@
             <td><input type="text" name="equipeb" value="" size="20" /></td>
         </tr>
         <tr>
-            <td><label>Joueur A</label></td>
-            <td><input type="text" name="joueura" value="" size="20" /></td>
-            <td><label>Joueur X</label></td>
-            <td><input type="text" name="joueurx" value="" size="20" /></td>
-        </tr>
-        <tr>
-            <td><label>Joueur B</label></td>
-            <td><input type="text" name="joueurb" value="" size="20" /></td>
-            <td><label>Joueur Y</label></td>
-            <td><input type="text" name="joueury" value="" size="20" /></td>
-        </tr>
-        <tr>
-            <td><label>Joueur C</label></td>
-            <td><input type="text" name="joueurc" value="" size="20" /></td>
-            <td><label>Joueur Z</label></td>
-            <td><input type="text" name="joueurz" value="" size="20" /></td>
-        </tr>
-        <tr>
-        <tr>
             <td><label>Score Equipe A</label></td>
             <td><input type="text" name="scoreea" value="" size="20" /></td>
             <td><label>Score Equipe B</label></td>
