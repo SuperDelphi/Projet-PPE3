d29432934c3efe6495336f6546d01e6fda5cb343
kemkemm <kaezer17390@gmail.com>
2019-11-20 15:23:47 +0100

    1

--- model/EquipeRencontreA.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once("Personne.php");
-
-class EquipeRencontreA extends Personne
-{
-    var $table = "personne inner join joueur on personne.idPersonne = joueur.idJoueur "
-            . "inner join equipe on joueur.idEquipe = equipe.idEquipe "
-            . "inner join rencontre on equipe.idEquipe = rencontre.idEquipeA";
-}
--- view/admin/formRencontre.php
+++ view/admin/formRencontre.php
@@ -1,9 +1,8 @@
-<br> <!== <?= BASE_URL . DS ?>admin/formRencontre2 !>
+<br>
 <table>
     <form method="POST" action="">
         <thead>
-            <th><label>Nouvelle Rencontre 2</label></th>
-        <th><label>Nouvelle Rencontre 1</label></th>
+            <th><label>Nouvelle Rencontre</label></th>
         </thead>
         <tr>
             <td><label>Heure</label></td>
@@ -15,21 +14,57 @@
         </tr>  
         <tr>
             <td><label>Equipe A</label></td>
-            <td><input type="text" name="equipea" value="" size="20" /></td>
+            <td>
+                <select name="equipea" required>
+                    <?php
+                    foreach ($equipes as $equipe) : ?>
+                        <option value="<?= $equipe->idEquipe ?>">
+                            <?= $equipe->nomEquipe ?>
+                        </option>
+                    <?php endforeach; ?>
+                </select>
+            </td>
             <td><label>Equipe B</label></td>
-            <td><input type="text" name="equipeb" value="" size="20" /></td>
+            <td>
+                <select name="equipeb" required>
+                    <?php
+                    foreach ($equipes as $equipe) : ?>
+                        <option value="<?= $equipe->idEquipe ?>">
+                            <?= $equipe->nomEquipe ?>
+                        </option>
+                    <?php endforeach; ?>
+                </select>
+            </td>
         </tr>
         <tr>
             <td><label>Score Equipe A</label></td>
-            <td><input type="text" name="scoreea" value="" size="20" /></td>
+            <td><input type="number" name="scorea" value="" size="20" /></td>
             <td><label>Score Equipe B</label></td>
-            <td><input type="text" name="scoreeb" value="" size="20" /></td> 
+            <td><input type="number" name="scoreb" value="" size="20" /></td> 
         </tr>
         <tr>
             <td><label>Journee</label></td>
-            <td><input type="text" name="journee" value="" size="20" /></td>
+            <td>
+            <select name="journee" required>
+                    <?php
+                    foreach ($journees as $journee) : ?>
+                        <option value="<?= $journee->idJournee ?>">
+                            <?= $journee->idJournee ?>
+                        </option>
+                    <?php endforeach; ?>
+                </select>
+            </td>
             <td><label>Arbitre</label></td>
-            <td><input type="text" name="arbitre" value="" size="20" /></td> 
+            <td>
+            <select name="arbitre" required>
+                    <?php
+                    foreach ($arbitres as $arbitre) : ?>
+                        <option value="<?= $arbitre->idArbitre ?>">
+                            <?= $arbitre->nom ?>
+                        </option>
+                    <?php endforeach; ?>
+                </select>
+            </td>
         </tr>
         <tr>
             <td><input type="submit" value="Terminer" name="creerrencontre" /></td>
