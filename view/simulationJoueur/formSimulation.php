1a2c1188b611eecce0e45bbbfa9e8d4746b7477c
Risuro <risemaj@gmail.com>
2019-11-20 14:44:21 +0100

    SimulationJoueur
    Controle de la saisie du formulaire

--- view/simulationJoueur/formSimulation.php
+++ view/simulationJoueur/formSimulation.php
@@ -5,21 +5,21 @@
         </thead>
         <tr>
             <td><label>Nom Joueur A</label></td>
-            <td><input type="text" name="nomJoueurA" value="" size="20" /></td>
+            <td><input type="text" name="nomJoueurA" value="" size="20" required placeholder="JoueurA" /></td>
         </tr>
 
         <tr>
             <td><label>Classement Joueur A</label></td>
-            <td><input type="number" name="ClassementJoueurA" value="" size="20" /></td>
+            <td><input type="number" name="ClassementJoueurA" value="" size="20" min="500" required placeholder="500" /></td>
         </tr>
 
         <tr>
             <td><label>Nom Joueur B</label></td>
-            <td><input type="text" name="nomJoueurB" value="" size="20" /></td>
+            <td><input type="text" name="nomJoueurB" value="" size="20" required placeholder="JoueurB" /></td>
         </tr>
         <tr>
             <td><label>Classement Joueur B</label></td>
-            <td><input type="number" name="ClassementJoueurB" value="" size="20" /></td>
+            <td><input type="number" name="ClassementJoueurB" value="" size="20" min="500" required placeholder="500" /></td>
         </tr>
         <tr>
             <td><label>Gagnant</label></td>
