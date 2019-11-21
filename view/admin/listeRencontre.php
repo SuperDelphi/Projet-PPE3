22ff5d15c3a2a752f842a798edd12ecc822ef3cb
Raphabuleux <castillophotos@gmail.com>
2019-11-21 16:05:39 +0100

    Update listeRencontre.php

--- view/admin/listeRencontre.php
+++ view/admin/listeRencontre.php
@@ -1,15 +1,33 @@
 <h2><?= $championnat->nomChampionnat . '<br>' . $championnat->typeChampionnat . '<br>' . $championnat->nomDivision; ?></h2>
 
 <?php
-$cpt = 0; 
-foreach ($rencontre as $r) : 
+$cpt = 0;
+foreach ($rencontre as $r) :
     ?>
+    <table border='1' style='text-align:center' class="data-table">
+        <tr>
+            <td style='width:40%'><a href="<?php echo BASE_URL . '/rencontre/detail/' . $r->idRencontre; ?>">
+                    <?php
+                        foreach ($equipes as $e) {
+                            if ($e->idEquipe === $r->idEquipeA) {
+                                echo $e->nomEquipe;
+                            }
+                        }
+                        ?></a></td>
+            <td style='width:5%'> <?= isset($r->scoreFinalA) ? $r->scoreFinalA : '?' ?> </td>
+            <td style='width:5%'> - </td>
+            <td style='width:5%'> <?= isset($r->scoreFinalB) ? $r->scoreFinalB : '?' ?> </td>
+            <td style='width:40%'>
+                <?php
+                    foreach ($equipes as $e) {
+                        if ($e->idEquipe == $r->idEquipeB) {
+                            echo $e->nomEquipe;
+                        }
+                    }
+                    ?> </td>
 
-    <div style=''>
-
-        <a href=" <?= BASE_URL."/admin/formRencontre/". $r->idRencontre ?>" colspan="5"><?= $equipes[$cpt]->nomEquipe." ".$equipes[$cpt++]->nomEquipe ?></a>
-
-    </div>
+        </tr>
+    </table>
 <?php
 
 endforeach;
