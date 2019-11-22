e370fd3810a22ff92455634c258fc97cf4202444
StarMaster007 <55792130+StarMaster007@users.noreply.github.com>
2019-11-22 08:57:49 +0100

    FiX Affichage Icon onglet

--- view/layout/default.php
+++ view/layout/default.php
@@ -4,8 +4,8 @@
 
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" charset=UTF-8">
-    <title><?= isset($title_for_layout) ? $title_for_layout : "SANA : Tennis de table 17"; ?></title>
-    <link rel="icon" href="favicon.ico" type="image/x-icon"/>
+    <title><?= isset($title_for_layout) ? $title_for_layout : "Tennis de table 17"; ?></title>
+    <link rel="icon" href="<?= BASE_SITE . DS . "img/logo_site.png" ?>" type="image/x-icon"/>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="<?= BASE_SITE . DS . 'css/fonts.css' ?>">
     <link rel="stylesheet" href="<?= BASE_SITE . DS . 'css/main.css' ?>">
