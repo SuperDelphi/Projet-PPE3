5b2a2aa2e7bba7db8280457a5d6fa1d7a7a1e186
Simon <simon.breil42@gmail.com>
2019-11-22 12:52:49 +0100

    Panel admin

--- view/layout/admin_menu.php
+++ view/layout/admin_menu.php
@@ -1,4 +1,5 @@
 <div class="container-fluid admin-menu col-lg-2">
+    <h4 class="admin-menu-title">Panel de gestion</h4>
     <a href="<?= BASE_URL . DS ?>admin/formChampionnat">
         <div class="row mx-0">
             <div class="col nav-item">Nouveau championnat</div>
@@ -14,7 +15,7 @@
             <div class="col nav-item">Nouvelle journée</div>
         </div>
     </a>
-    <br>
+    <hr>
     <a href="<?= BASE_URL . DS ?>auth/logout">
         <div class="row mx-0">
             <div class="col nav-item">Équipes</div>
@@ -30,7 +31,7 @@
             <div class="col nav-item">Utilisateurs</div>
         </div>
     </a>
-    <br>
+    <hr>
     <a class="disconnect-link" href="<?= BASE_URL . DS ?>auth/logout">
         <div class="row mx-0">
             <div class="col nav-item">Déconnexion</div>
