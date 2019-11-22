ff896d44be4fa29124767fdb45306b835e3bd5bf
StarMaster007 <55792130+StarMaster007@users.noreply.github.com>
2019-11-22 08:49:28 +0100

    FIX Affichage liste EquipePoule

--- view/rencontre/listeEquipePoule.php
+++ view/rencontre/listeEquipePoule.php
@@ -1,7 +1,9 @@
 <h2><?= $championnat->nomChampionnat . ' ' . $championnat->typeChampionnat . ' ' . $championnat->nomDivision; ?></h2>
 <h3>Classement des équipes</h3>
 <div>
-<?php if (isset($poule)) { echo '<h3>Poule '. $poule[0]->nomPoule . '</h3>';} ?>
+<?php if (isset($poule)) {
+    echo '<h3>Poule ' . $poule[0]->nomPoule . '</h3>';
+} ?>
     <table border=1 class="data-table">
     <thead>
         <th>Place</th>
@@ -9,18 +11,10 @@
         <th>Scores</th>
     </thead>
     <?php $place = 1;
-    if (isset($poule)) {
-        foreach ($equipesPoules[0] as $equipe) {
-            echo '<tr><td>' . $place++ . '</td>
+    foreach ($equipesPoules[0] as $equipe) {
+        echo '<tr><td>' . $place++ . '</td>
             <td>' . $equipe->nomEquipe . '</td>
             <td>' . $equipe->scoreGlobal . '</td></tr>';
-        }
-    } else {
-        foreach ($equipesPoules[0] as $equipe) {
-            echo '<tr><td>' . $place++ . '</td>
-            <td>' . $equipe->nomEquipe . '</td>
-            <td>' . $equipe->scoreGlobal . '</td></tr>';
-        }
     }
     ?>
     </table>
