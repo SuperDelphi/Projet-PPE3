666d7a488eaa7f09a355ae159907c5957ac9066a
Simon <simon.breil42@gmail.com>
2019-11-23 16:03:44 +0100

    Signature

--- view/layout/footer.php
+++ view/layout/footer.php
@@ -13,14 +13,15 @@
                     <!-- Links -->
                     <h5 class="text-uppercase">Liens utiles</h5>
 
-<!--                    <div class="horizontal-sep"></div>-->
-<hr class="">
+                    <!--                    <div class="horizontal-sep"></div>-->
+                    <hr class="">
                     <ul class="list-unstyled">
                         <li>
                             <a href="mailto:ufolep-usep@laligue17.org" class="link footer-text">Contact</a>
                         </li>
                         <li>
-                            <a href="<?= BASE_URL . DS . "info/rgpd" ?>" class="link footer-text">Politique de confidentialité</a>
+                            <a href="<?= BASE_URL . DS . "info/rgpd" ?>" class="link footer-text">Politique de
+                                confidentialité</a>
                         </li>
 
                     </ul>
@@ -34,15 +35,17 @@
                     <!-- Links -->
                     <h5 class="text-uppercase">Plan du site</h5>
 
-<!--                    <div class="horizontal-sep"></div>-->
+                    <!--                    <div class="horizontal-sep"></div>-->
                     <hr>
 
                     <ul class="list-unstyled">
                         <li>
-                            <a href="<?= BASE_URL . DS . "championnat/liste" ?>" class="link footer-text">Championnats</a>
+                            <a href="<?= BASE_URL . DS . "championnat/liste" ?>"
+                               class="link footer-text">Championnats</a>
                         </li>
                         <li>
-                            <a href="<?= BASE_URL . DS . "joueur/listeGenerale" ?>" class="link footer-text">Classement des joueurs</a>
+                            <a href="<?= BASE_URL . DS . "joueur/listeGenerale" ?>" class="link footer-text">Classement
+                                des joueurs</a>
                         </li>
                         <li>
                             <a href="<?= BASE_URL . DS . "auth/login" ?>" class="link footer-text">Connexion</a>
@@ -57,10 +60,21 @@
                     <!-- Content -->
                     <h5 class="text-uppercase" style="text-align:right;">L'UFOLEP 17 </h5>
 
-                    <p class="footer-text text-right">Résidence Club La Fayette, Avenue de Bourgogne 17401 La Rochelle (CEDEX
+                    <p class="footer-text text-right">Résidence Club La Fayette, Avenue de Bourgogne 17401 La Rochelle
+                        (CEDEX
                         01)
                         <br>05 46 41 73 13
-                        <br>ufolep-usep@laligue17.org</p>
+                        <br>ufolep-usep@laligue17.org
+                        <br><br>
+                        Réalisé avec <i class="heart-shape"></i> par le BTS SIO de Rochefort.
+                        <br>
+                        <span style="color: rgba(255, 255, 255, 0.5)">
+                            <span style="color:rgba(255, 79, 79, 0.75)">Hugo</span>,
+                            <span style="color:rgba(210, 255, 47, 0.75)">Simon</span>,
+                            <span style="color:rgba(75, 177, 121, 0.75)">Gabriel</span>,
+                            <span style="color:rgba(75, 123, 255, 0.75)">Raphaël</span> et
+                            <span style="color:rgba(255, 107, 223, 0.75)">Léo</span>
+                    </p>
                     <br>
                 </div>
             </div>
